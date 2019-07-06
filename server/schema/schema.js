8ece0ca9161f2f3b41c2dde1ed47a3e9747d2957
David De Wulf <ledavid64@gmail.com>
2019-07-06 09:10:48 -0700

    GraphQL ID Type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,8 @@
 const graphql = require('graphql')
 const _ = require('lodash')
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql
+
 // dummy data
 const books = [
   { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
@@ -12,7 +13,7 @@ const books = [
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
@@ -23,7 +24,7 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data fom db / other source
         return _.find(books, { id: args.id })
