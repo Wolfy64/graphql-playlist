7db98cc7003b3dea2ea2abb1b64a9bd4f020e93a
David De Wulf <ledavid64@gmail.com>
2019-07-06 00:44:14 -0700

    The Resolve Function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+// dummy data
+const books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+]
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -19,6 +26,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // code to get data fom db / other source
+        return _find(books, { id: args.id })
       }
     }
   }
