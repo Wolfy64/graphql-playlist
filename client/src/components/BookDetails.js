e8cc322c81b44cacc30204fe5580f244efaed8bd
David De Wulf <ledavid64@gmail.com>
2019-07-08 02:08:35 -0700

    Making a Single Query

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,19 +1,43 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { graphql } from 'react-apollo'
 
 import { GET_BOOK } from '../queries/queries'
 
-function BookDetails({ data }) {
-  // const [name, setName] = useState('')
-  // const { loading, authors } = data
-
-  // if (loading) return 'Loading authors...'
-
+const Details = ({ book }) => {
+  if (!book) return null
   return (
-    <div id="book-details">
-      <p>Ouput book details here</p>
+    <div>
+      <h2>{book.name}</h2>
+      <p>{book.genre}</p>
+      <p>{book.author.name}</p>
+      <p>All books by this author:</p>
+      <ul className="other-books">
+        {book.author.books.map(e => (
+          <li key={e.id}>{e.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
 
-export default graphql(GET_BOOK)(BookDetails)
+function BookDetails({ bookId, data }) {
+  const { loading, book } = data
+  if (loading) return 'Loading details..'
+
+  return (
+    <div id="book-details">
+      <p>Ouput book details here</p>
+      <Details book={book} />
+    </div>
+  )
+}
+
+export default graphql(GET_BOOK, {
+  options: ({ bookId }) => {
+    return {
+      variables: {
+        id: bookId
+      }
+    }
+  }
+})(BookDetails)
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql } from 'react-apollo'
 
 import { GET_BOOKS } from '../queries/queries'
 import BookDetails from './BookDetails'
 
 function BookList({ data }) {
+  const [selected, setSelected] = useState(null)
   const { loading, books } = data
   if (loading) return 'Loading books..'
 
-  const booksList = books.map(e => <li key={e.id}>{e.name}</li>)
+  const booksList = books.map(e => (
+    <li key={e.id} onClick={() => setSelected(e.id)}>
+      {e.name}
+    </li>
+  ))
   return (
     <div>
       <ul id="book-list">{booksList}</ul>
-      <BookDetails />
+      <BookDetails bookId={selected} />
     </div>
   )
 }
