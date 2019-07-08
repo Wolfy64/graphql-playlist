3889cd01a29aaa6f12a0367cceaec09f51a89bda
David De Wulf <ledavid64@gmail.com>
2019-07-08 00:30:59 -0700

    Updating Component State

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,31 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql } from 'react-apollo'
 
 import { GET_AUTHORS } from '../queries/queries'
 
 function AddBook({ data }) {
+  const [name, setName] = useState('')
+  const [genre, setGenre] = useState('')
+  const [authorId, setAuthorId] = useState('')
   const { loading, authors } = data
+
   if (loading) return 'Loading authors...'
 
-  const authorsList = authors.map(e => <option key={e.id}>{e.name}</option>)
+  const authorsList = authors.map(e => (
+    <option key={e.id} value={e.id}>
+      {e.name}
+    </option>
+  ))
+
+  const submit = e => {
+    e.preventDefault()
+    console.log('submit', { name, genre, authorId })
+  }
+
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={submit}>
       <div className="field">
         <label>Book name:</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
       </div>
       <div className="field">
         <label>Genre:</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={genre}
+          onChange={e => setGenre(e.target.value)}
+        />
       </div>
       <div className="field">
         <label>Author:</label>
-        <select>
+        <select value={authorId} onChange={e => setAuthorId(e.target.value)}>
           <option>Select author</option>
           {authorsList}
         </select>
       </div>
-      <button>+</button>
+      <button>Add Book</button>
     </form>
   )
 }
